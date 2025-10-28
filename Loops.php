ed5e9d0e7408264eaea50c191e968322825a09d9
g.valdesrodrguez@ecoles-epsi.net <g.valdesrodrguez@ecoles-epsi.net>
2025-10-28 16:53:18 +0100

    Commence a tester PDO

--- Loops.php
+++ Loops.php
@@ -1,14 +1,28 @@
 <?php
-$dsn = 'mysql:host=localhost;http://localhost/phpmyadmin/index.php?route=/database/structure&db=agence+de+tourisme';
-$username = 'root';
-$password = '';
+// Configuration de base de données
+$host = "localhost"; // Serveur
+$user = "root"; // Utilisateur  MySQL
+$password = ""; // Le mot de passe de MySQL
+$base_de_donnee = "agence de tourisme";// Le nom de la base de données
 
+// Connexion à la base de données
 try {
-    $pdo = new PDO($dsn, $username, $password);
-    // Set error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully!";
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    $conn = new PDO("mysql:host=$host;dbname=$base_de_donnee", $user, $password);
+    echo "Connected to $base_de_donnee at $host successfully.";
+} catch (PDOException $pe) {
+    die("Could not connect to the database $base_de_donnee :" . $pe->getMessage());
 }
+
+// Insérer donnée dans la table
+$sql = "INSERT INTO type (Type) VALUES ('test')";
+$sql= "INSERT INTO type (Type) VALUES ('test1')";
+$sql= "INSERT INTO type (Type) VALUES ('essai')";
+
+
+
+if ($conn->query($sql) === TRUE) {
+    echo " Données enregistrées correctement.";
+}
+
+
 ?>
