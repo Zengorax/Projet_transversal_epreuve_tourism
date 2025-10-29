d5974b2badeb6a438762c7bc6b42dcd5143b4e7f
HdEX <doriankch@gmail.com>
2025-10-29 16:39:48 +0100

    liaison de toutes les pages avec la nouvelle page voyage.php

--- app/voyages.php
+++ app/voyages.php
@@ -203,8 +203,8 @@
             <h3 class="mb-0">Horizon Sportif</h3>
             <nav class="nav nav-masthead justify-content-center position-absolute start-50 translate-middle-x">
                 <a class="nav-link fw-bold py-1 px-2" aria-current="page" href="./index.html">Accueil</a>
-                <a class="nav-link fw-bold py-1 px-2 active" href="./voyage.html">Voyages</a>
-                <a class="nav-link fw-bold py-1 px-2" href="#">Contact</a>
+                <a class="nav-link fw-bold py-1 px-2 active" href="./voyages.php">Voyages</a>
+                <a class="nav-link fw-bold py-1 px-2" href="./Contact.html">Contact</a>
             </nav>
             <div class="dropdown ms-auto">
                 <a href="#" class="d-flex align-items-center link-body-emphasis text-decoration-none dropdown-toggle"
