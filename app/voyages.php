accc1d5e1d7a305f2ff00ddd991a8f07160b6bb9
HdEX <doriankch@gmail.com>
2025-11-20 12:22:35 +0100

    correction de petits problemes

--- app/voyages.php
+++ app/voyages.php
@@ -303,7 +303,7 @@
     </div>
     <footer class="text-body-secondary py-5">
         <div class="container">
-            <p class="float-end mb-1"><a href="#">Back to top</a></p>
+            <p class="mb-1"><a href="#">Back to top</a></p>
         </div>
     </footer>
     <script src="./js/theme-toggle.js"></script>
